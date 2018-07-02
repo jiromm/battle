009f949b7ef44fafed2364ce7686881e9a15d922
Aram Baghdasaryan <jiromm@users.noreply.github.com>
2018-07-02 08:46:49 +0400

    Update Result.php

--- src/Result.php
+++ src/Result.php
@@ -8,8 +8,6 @@ class Result
 
     public function __construct(Army $army1, Army $army2)
     {
-//        echo sprintf('Total Health: %s - %s', $army1->getTotalHealth(), $army2->getTotalHealth()) . PHP_EOL;
-
         if ($army1->getTotalHealth()) {
             $this->winner = $army1;
         } elseif ($army2->getTotalHealth()) {
